8e9296d59bd223d06fdd21301acd5202333752ba
mummy-alive <mummyee@ewhain.net>
2024-07-07 01:28:25 +0900

    버그 수정. 게시판 화면 백엔드 기본 구성.

--- teamfinder_project/teamfinder/urls.py
+++ teamfinder_project/teamfinder/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 import os
-import views
+from teamfinder import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
-    path('onboard/', views.onboard, name='onboard')
+    path('', views.index, name='index'),    # 첫화면
+    path('admin/', admin.site.urls),    # admin 페이지
+    path('onboard/', views.onboard, name='onboard'),
+    path('post/', views.posts_home, name='post'),        # 게시글 목록
+    path('post/<int:pk>', views.posts_page, name="postpage")     # 게시글 상세
 ]
